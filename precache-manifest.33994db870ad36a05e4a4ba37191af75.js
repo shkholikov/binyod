48906975771ebe4071feee20387dd08507a665ba
Shakhzod Kholikov <imshakhzod@gmail.com>
2020-07-31 20:57:24 +0200

    Updates

---
+++ precache-manifest.33994db870ad36a05e4a4ba37191af75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcd11acec927ace62d175098e687a1b1",
+    "url": "/Shakhzodzhan/binyod-website.git/index.html"
+  },
+  {
+    "revision": "0321ac93e95b18bbefd2",
+    "url": "/Shakhzodzhan/binyod-website.git/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "232c2d69a33b322d2e7a",
+    "url": "/Shakhzodzhan/binyod-website.git/static/js/2.a9f60531.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Shakhzodzhan/binyod-website.git/static/js/2.a9f60531.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0321ac93e95b18bbefd2",
+    "url": "/Shakhzodzhan/binyod-website.git/static/js/main.2f224c12.chunk.js"
+  },
+  {
+    "revision": "03325de615562b12f25f",
+    "url": "/Shakhzodzhan/binyod-website.git/static/js/runtime-main.ce2cbc7e.js"
+  },
+  {
+    "revision": "4d295ea8ae0553dda39ffb8b66672db9",
+    "url": "/Shakhzodzhan/binyod-website.git/static/media/binyod.4d295ea8.png"
+  }
+]);
